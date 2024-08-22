17c6d211dee68a9581b7a30c339750cf0e428aab
Abel0001 <abel.words1@gmail.com>
2024-08-22 22:39:44 +0200

    Music, trying to fix stuff

--- Hole In the Wall.js
+++ Hole In the Wall.js
@@ -7,10 +7,48 @@ https://sprig.hackclub.com/gallery/getting_started
 @tags: []
 @addedOn: 2024-00-00
 */
+let countdownTimeout;
 
 const player = "p"
 const filled = "w"
 const regen = "r"
+const extratime = "x"
+const lowerdiff = "l"
+
+const melody = tune`
+454.54545454545456: C4^454.54545454545456 + E4~454.54545454545456,
+454.54545454545456: C5~454.54545454545456 + E4~454.54545454545456 + E5^454.54545454545456 + G5-454.54545454545456 + C4~454.54545454545456,
+454.54545454545456: E4~454.54545454545456 + C4~454.54545454545456,
+454.54545454545456: D5~454.54545454545456 + E4~454.54545454545456 + C4~454.54545454545456 + G4-454.54545454545456 + B4/454.54545454545456,
+454.54545454545456: E4~454.54545454545456 + C4~454.54545454545456,
+454.54545454545456: C4~454.54545454545456 + A4~454.54545454545456 + C5~454.54545454545456,
+454.54545454545456: C4~454.54545454545456 + E4~454.54545454545456,
+454.54545454545456: E4~454.54545454545456 + B4~454.54545454545456 + G4~454.54545454545456,
+454.54545454545456: C5~454.54545454545456 + F4~454.54545454545456,
+454.54545454545456: E4~454.54545454545456,
+454.54545454545456: E4^454.54545454545456 + G4-454.54545454545456 + C4~454.54545454545456 + B4-454.54545454545456,
+454.54545454545456: E4^454.54545454545456 + C4~454.54545454545456,
+454.54545454545456: E4^454.54545454545456 + C4~454.54545454545456 + C5~454.54545454545456 + A4~454.54545454545456,
+454.54545454545456: C4~454.54545454545456,
+454.54545454545456: C4~454.54545454545456 + G4/454.54545454545456 + B4/454.54545454545456,
+454.54545454545456: C4~454.54545454545456,
+454.54545454545456: B4~454.54545454545456 + G4~454.54545454545456 + E4~454.54545454545456,
+454.54545454545456,
+454.54545454545456: A4~454.54545454545456 + C5~454.54545454545456 + E5~454.54545454545456 + C4^454.54545454545456,
+454.54545454545456: C4^454.54545454545456,
+454.54545454545456: C5~454.54545454545456 + A4~454.54545454545456 + F4~454.54545454545456 + C4^454.54545454545456,
+454.54545454545456: C4^454.54545454545456,
+454.54545454545456: B4~454.54545454545456 + D5~454.54545454545456 + F5~454.54545454545456 + C4^454.54545454545456,
+454.54545454545456: C4^454.54545454545456,
+454.54545454545456: B4~454.54545454545456 + D5~454.54545454545456 + G4-454.54545454545456,
+454.54545454545456,
+454.54545454545456: G4~454.54545454545456 + E4/454.54545454545456,
+454.54545454545456,
+454.54545454545456: E4/454.54545454545456,
+454.54545454545456: G4/454.54545454545456,
+454.54545454545456: B4/454.54545454545456,
+454.54545454545456: C4-454.54545454545456 + E4~454.54545454545456 + G4^454.54545454545456`
+
 var roundCounter = 0;
 var powerUps = []
 var stopCountdown = false;
@@ -36,20 +74,53 @@ setLegend(
 ................
 ................
 ................
-.....444444.....
-....4......4....
-...4............
-...4........4...
-...4.......444..
-...4......4.4.4.
-...4........4...
-...4........4...
-....4......4....
-.....444444.....
+.....000000.....
+....0......0....
+...0............
+...0........0...
+...0.......000..
+...0......0.0.0.
+...0........0...
+...0........0...
+....0......0....
+.....000000.....
 ................
 ................
 ................`],
-
+  [extratime, bitmap`
+................
+................
+................
+....00000000....
+....0......0....
+.....011110.....
+......0110......
+.......00.......
+.......00.......
+......0..0......
+.....0....0.....
+....01111110....
+....00000000....
+................
+................
+................`],
+  [lowerdiff, bitmap`
+................
+................
+................
+................
+.......00.......
+.......00.......
+.......00.......
+.......00.......
+....0..00..0....
+....0..00..0....
+.....0.00.0.....
+......0000......
+.......00.......
+................
+................
+................`],
   [filled, bitmap`
 1111111111111111
 1CCCCCCCCCCCCCC1
@@ -77,8 +148,17 @@ var level = map`
 .....
 .....
 .....`
+function killCountdown() {
+    stopCountdown = true;
 
+    countDownValue = 0;
+
+  if (countdownTimeout) {
+    clearTimeout(countdownTimeout);
+  }
+}
 function generateLevel() {
+      killCountdown()
   clearText()
   var powerUpCount = 0;
   roundCounter++;
@@ -108,9 +188,17 @@ function generateLevel() {
       if (powerUpCount < 2) {
         if (Math.floor(Math.random() * 6) == 1) {
           powerUpCount++;
-          switch (Math.floor(Math.random() * 1)) {
+          switch (Math.floor(Math.random() * 3)) {
             case 0:
               addSprite(i % 5, Math.floor(i / 5), regen)
+
+              break;
+            case 1:
+              addSprite(i % 5, Math.floor(i / 5), extratime)
+
+              break;
+            case 2:
+              addSprite(i % 5, Math.floor(i / 5), lowerdiff)
 
               break;
           }
@@ -122,10 +210,10 @@ function generateLevel() {
 
   }
   checkLevel(true)
-constructPowerUpString()
-    stopCountdown = false
-startCountdown(8) 
-
+  constructPowerUpString()
+  stopCountdown = false
+  startCountdown(8)
+pickPowerUp()
 }
 
 var playerPos = Math.floor(Math.random() * 30);
@@ -136,7 +224,7 @@ var difficulty = 1 + roundCounter;
 
 generateLevel();
 
-
+var countDownValue;
 
 function pickPowerUp() {
   if (powerUps.length < 3) {
@@ -149,69 +237,105 @@ function pickPowerUp() {
       addSprite(pX, pY, filled)
       powerUps += "r";
     }
+    if (getTile(getFirst(player).x, getFirst(player).y).some(sprite => sprite.type == extratime)) {
+      clearTile(pX, pY)
+      addSprite(pX, pY, player)
+      addSprite(pX, pY, filled)
+      powerUps += "x";
+    }
+    if (getTile(getFirst(player).x, getFirst(player).y).some(sprite => sprite.type == lowerdiff)) {
+      clearTile(pX, pY)
+      addSprite(pX, pY, player)
+      addSprite(pX, pY, filled)
+      powerUps += "l";
+    }
   }
   constructPowerUpString()
 }
 //First ability
 
-function constructPowerUpString(){
+function constructPowerUpString() {
   var string = "Powerups:"
-  for(var i = 0; i < powerUps.length; i++){
+  for (var i = 0; i < powerUps.length; i++) {
     string += powerUps[i] + " "
   }
-clearText();
-   addText(string, {
+  clearText();
+  addText(string, {
     x: 3,
     y: 14,
     color: color`5`
   })
+  if (countDownValue != 0 || stopCountdown == false) {
+    addText(`Countdown: ` + countDownValue, {
+      x: 4,
+      y: 2,
+      color: color`5`
+    })
+  }
 }
 
 
-function usePowerUps(aN){
-  if(aN > powerUps.length - 1 ) return;
-  if(powerUps[aN] == "r"){
+function usePowerUps(aN) {
+  if (aN > powerUps.length - 1) return;
+  if (powerUps[aN] == "r") {
+
     generateLevel()
-        difficulty -= 1;
+    difficulty -= 1;
+    roundCounter -= 1;
 
   }
+  if (powerUps[aN] == "l") {
+    difficulty -= 4;
 
+    generateLevel()
 
-  if(powerUps.length == 1){
+    roundCounter -= 1;
+
+  }
+  if (powerUps[aN] == "x") {
+    
+    let timeBefore = countDownValue;
+    killCountdown()
+
+  stopCountdown = false
+  startCountdown(8 + countDownValue)
+  }
+
+  if (powerUps.length == 1) {
     powerUps = []
   }
 
-    if(powerUps.length == 2){
-      if(aN == 0)
-          powerUps = [powerUps[aN + 1]];
-      if(aN == 1)
-          powerUps = [powerUps[aN - 1]];
-  } 
-  if(powerUps.length == 3){
-      if(aN == 0)
-          powerUps = [powerUps[aN + 1], powerUps[aN + 2]];
-      if(aN == 1)
-          powerUps = [powerUps[aN - 1], powerUps[aN+ 1]];
-     if(aN == 2)
-          powerUps = [powerUps[aN - 2], powerUps[aN - 1]];
-  } 
-  
-constructPowerUpString()
+  if (powerUps.length == 2) {
+    if (aN == 0)
+      powerUps = [powerUps[aN + 1]];
+    if (aN == 1)
+      powerUps = [powerUps[aN - 1]];
+  }
+  if (powerUps.length == 3) {
+    if (aN == 0)
+      powerUps = [powerUps[aN + 1], powerUps[aN + 2]];
+    if (aN == 1)
+      powerUps = [powerUps[aN - 1], powerUps[aN + 1]];
+    if (aN == 2)
+      powerUps = [powerUps[aN - 2], powerUps[aN - 1]];
+  }
+
+  constructPowerUpString()
 }
 
 onInput("j", () => {
-usePowerUps(0)
+  usePowerUps(0)
 })
 //Second ability
 
 onInput("k", () => {
-usePowerUps(1)
+  usePowerUps(1)
 
 })
 //Third ability
 
 onInput("l", () => {
-usePowerUps(2)
+  usePowerUps(2)
 
 })
 getFirst(player).x
@@ -256,6 +380,7 @@ function checkLevel(isGenerated) {
   if (isGenerated) {
     generateLevel();
     roundCounter -= 1;
+    difficulty -= 1;
     return;
   }
   clearText()
@@ -270,43 +395,52 @@ function checkLevel(isGenerated) {
 }
 async function startCountdown(seconds) {
   for (let i = seconds; i > 0; i--) {
-    if(stopCountdown) return;
+    if (stopCountdown) { killCountdown();
+                        i = -4;
+      break;}
     clearText()
+    countDownValue = i;
+    constructPowerUpString();
     addText(`Countdown: ${i}`, {
       x: 4,
       y: 2,
       color: color`5`
     })
-    await new Promise(resolve => setTimeout(resolve, 1000)) 
-  }if(!stopCountdown){
-  clearText()
-  addText("Fail!\n Score: "+ roundCounter - 1, {
-    x: 4,
-    y: 2,
-    color: color`5`
-  })
-setTimeout(() => {
+    await new Promise(resolve => {
+      countdownTimeout = setTimeout(resolve, 1000);
+    });
+  }
+
+  if (!stopCountdown) {
+    clearText()
+    addText("Fail!\n Score: " + (roundCounter - 1), {
+      x: 4,
+      y: 2,
+      color: color`5`
+    });
+
+    await new Promise(resolve => setTimeout(resolve, 3000))
     resetGame()
-  }, 550)
+  }
 }
-}
-function resetGame(){
-  
-  var roundCounter = 0;
-var powerUps = []
-var difficulty = 1 + roundCounter;
-  var level = map`
+
+function resetGame() {
+
+  roundCounter = 0;
+  powerUps = []
+  difficulty = 1 + roundCounter;
+  level = map`
 .....
 .....
 .....
 .....
 .....
 .....`
-generateLevel()
-  stopCountdown = true
+  generateLevel()
 }
 
-startCountdown(8) 
+
+
 
 afterInput(() => {
 
